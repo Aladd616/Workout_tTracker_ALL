00e5e8c2b400d2a2be6d98e427a1d6583f2ba48a
Allen Ladd <ghost2130@gmail.com>
2021-09-11 03:01:18 -0400

    probbaly finished schema

--- workoutModel.js
+++ workoutModel.js
@@ -5,14 +5,39 @@ const workoutSchema = new Schema({
   name: {
     type: String,
     trim: true,
-    required: "Enter a name for this workout",
+    // required: "Enter a name for this workout",
   },
   cardio_name: {
     type: String,
     trim: true,
-    required: "Enter a name for this Cardio session",
+    // required: "Enter a name for this Cardio session",
   },
-  type: {},
+  type: {
+    type: String,
+    trim: true,
+    enum: {
+      values: ["cardio", "resistance"],
+      message: "{VALUE} is not supported",
+    },
+    sets: {
+      type: Number,
+    },
+    distance: {
+      type: Number,
+    },
+    duration: {
+      type: Number,
+    },
+    reps: {
+      type: Number,
+    },
+    resistance_duration: {
+      type: Number,
+    },
+    weight: {
+      type: Number,
+    },
+  },
 });
 
 const workout = mongoose.model("workout", workoutSchema);
