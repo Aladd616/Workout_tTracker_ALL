8be787d4ca3b239de3520d8b589a997258251fa9
Allen Ladd <ghost2130@gmail.com>
2021-09-11 12:09:25 -0400

    mostly finished schema added index.js to model

---
+++ models/workout.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now,
+  },
+  exercises: [
+    {
+      type: {
+        type: String,
+        trim: true,
+        required: "please name a type of workout",
+      },
+
+      name: {
+        type: String,
+        trim: true,
+        required: "Enter a name for this workout",
+      },
+
+      sets: {
+        type: Number,
+      },
+      distance: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      duration: {
+        type: Number,
+        required: "enter exercise duration in minutes",
+      },
+      weight: {
+        type: Number,
+      },
+    },
+  ],
+});
+
+const workout = mongoose.model("workout", workoutSchema);
+
+module.exports = workout;
