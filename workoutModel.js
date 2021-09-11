5ac23c07281fd60d2af07d66ae68fa9b29864005
Allen Ladd <ghost2130@gmail.com>
2021-09-11 02:29:37 -0400

    added some of the schema and the start of the server

--- workoutModel.js
+++ workoutModel.js
@@ -2,8 +2,17 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-    
-
+  name: {
+    type: String,
+    trim: true,
+    required: "Enter a name for this workout",
+  },
+  cardio_name: {
+    type: String,
+    trim: true,
+    required: "Enter a name for this Cardio session",
+  },
+  type: {},
 });
 
 const workout = mongoose.model("workout", workoutSchema);
