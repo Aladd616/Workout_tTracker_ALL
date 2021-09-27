9c29a328c845f2588392a7b3521a637a8c617b1e
Allen Ladd <ghost2130@gmail.com>
2021-09-26 23:47:20 -0400

    added html routes

--- routes/html.js
+++ routes/html.js
@@ -1,3 +1,16 @@
 const router = require("express").Router();
+const path = require("path");
 
-module.export = router;
+module.exports = function (app) {
+  app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+  app.get("/exercise", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+  });
+  app.get("/stats", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+  });
+};
+
+module.exports = router;
